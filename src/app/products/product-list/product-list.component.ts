fd4b0a322d4330926887e164232d06f8c4cf1c2a
Tegridy Code <marcgabel@live.dk>
2021-11-16 00:58:15 +0100

    ui and onDestroy

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductsService} from "../shared/products.service";
 import {ProductDto} from "../shared/product.dto";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-ToMo-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   products: ProductDto[] | undefined;
+  private productSub: Subscription | undefined;
 
   constructor(private _productService: ProductsService) {
 
   }
 
   ngOnInit(): void {
-    this._productService.getAll().subscribe(products => {this.products = products;});
+    this.productSub = this._productService.getAll().subscribe(products => {this.products = products;});
+  }
+
+  ngOnDestroy(): void {
+    if (this.productSub){
+      this.productSub.unsubscribe();
+    }
   }
 
 }
